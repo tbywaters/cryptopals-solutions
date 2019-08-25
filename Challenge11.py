1082add1cc199709fa83ab1588ffd4fbbcec2544
tbywaters <timbywaters@gmail.com>
2019-08-25 15:17:04 +1000

    Forgot part of the challenge

--- Challenge11.py
+++ Challenge11.py
@@ -7,12 +7,21 @@ import cryptopalsmod.aes_ecb_attacks as ecb_attacks
 
 def encryption_orcle(plaintext):
     """Encrypts plaintext randomly using eith CBC or ECB with a random key.
-    Use for testing a function which detects which method is used. Returns a
-    tuple of either 'ECB' or 'CBC' depending on the method used and also the 
+    Pads each end of the plaintext with random bytes. Use for testing a 
+    function which detects which method is used. Returns a tuple of either 
+    'ECB' or 'CBC' (depending on the method used) and also the 
     ciphertext"""
-    
+
     IV = bytes(16*[0])
     key = secrets.token_bytes(16)
+    
+    #Randomly choose the amount of padding for the begining and end of the 
+    # plaintext 
+    front_pad_num = secrets.choice(range(5,10))
+    back_pad_num = secrets.choice(range(5,10))
+
+    plaintext = (secrets.token_bytes(front_pad_num) + plaintext +
+                            secrets.token_bytes(back_pad_num))
 
     cipher_mode = secrets.choice(['ECB', 'CBC'])
 
@@ -24,6 +33,9 @@ def encryption_orcle(plaintext):
     return cipher_mode, cipher.encrypt(plaintext)
 
 def cipher_mode_guesser(ciphertext):
+    """Determins whether a ciphertex has been encrypted using ECB or CBC by 
+    using ecb_attacks.detect_ECB which counts repeating blocks"""
+    
     if ecb_attacks.detect_ECB(ciphertext):
         return 'ECB'
     return 'CBC'
